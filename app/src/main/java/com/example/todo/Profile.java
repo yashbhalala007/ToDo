9f1295326061ff7f81cab8da4d928e86d9bfd092
Yash Bhalala <66874477+yashbhalala007@users.noreply.github.com>
2022-10-23 00:25:51 -0400

    edit profile

--- app/src/main/java/com/example/todo/Profile.java
+++ app/src/main/java/com/example/todo/Profile.java
@@ -1,14 +1,26 @@
 package com.example.todo;
 
+import static java.security.AccessController.getContext;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +37,8 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Profile extends AppCompatActivity {
@@ -38,6 +52,7 @@ public class Profile extends AppCompatActivity {
 
     private ProgressDialog loadingBar;
     final static int Gallery_Pick = 1;
+    private FragmentActivity myContex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +105,102 @@ public class Profile extends AppCompatActivity {
 
             }
         });
+
+        txt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateName();
+            }
+        });
+
+        txt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateEmail();
+            }
+        });
+    }
+
+    private void updateName() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Profile.this);
+        builder.setTitle("Name Update");
+
+        LinearLayout linearLayout = new LinearLayout(Profile.this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+
+        final EditText name = new EditText(Profile.this);
+        name.setLayoutParams(params);
+
+
+        linearLayout.addView(name);
+
+        builder.setView(linearLayout);
+
+        builder.setCancelable(false)
+                .setPositiveButton("UPDATE",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                mDatabase.child("Users").child(userid).child("name").setValue(name.getText().toString());
+                            }
+                        })
+                .setNegativeButton("CANCLE",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                // code to do on NO tapped
+                                dialog.cancel();
+                            }
+                        });
+        Dialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void updateEmail() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Profile.this);
+        builder.setTitle("Email Update");
+
+        LinearLayout linearLayout = new LinearLayout(Profile.this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+
+        final EditText email = new EditText(Profile.this);
+        email.setLayoutParams(params);
+
+
+        linearLayout.addView(email);
+
+        builder.setView(linearLayout);
+
+        builder.setCancelable(false)
+                .setPositiveButton("UPDATE",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+
+                                mAuth.getCurrentUser().updateEmail(email.getText().toString())
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        mDatabase.child("Users").child(userid).child("email").setValue(email.getText().toString());
+                                                    }
+                                                });
+
+                            }
+                        })
+                .setNegativeButton("CANCLE",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                // code to do on NO tapped
+                                dialog.cancel();
+                            }
+                        });
+        Dialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
